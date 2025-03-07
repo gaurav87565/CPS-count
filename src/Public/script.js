07f9ba3d6271246172f6ffd4a4b055449c52d0b8
Gaurav <81549793+gaurav87565@users.noreply.github.com>
2025-03-07 19:36:19 +0530

    updated files

---
+++ src/Public/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+	document.getElementById("menu").classList.toggle("active");
+}
+
+function startTest(duration) {
+	window.location.href = `src/Public/Pages/${duration}sec.html?duration=${duration}`;
+}
