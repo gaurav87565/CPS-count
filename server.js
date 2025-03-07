499f8b855742f6f7c99cd6578715a029bb89a6cb
Gaurav <81549793+gaurav87565@users.noreply.github.com>
2025-03-07 23:16:25 +0530

    w

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'Assets')));
 
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
